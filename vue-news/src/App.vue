0a78c6ae176e4a139550991d79208b52d1f87306
ratseno <onestar1014@gmail.com>
2019-07-17 23:35:38 +0900

    router-link -header component 작성 > TooBar

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <!-- Strongly Recommend -->
+    <tool-bar></tool-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from '../src/components/ToolBar.vue';
+
 export default {
-  
+  components: {
+    ToolBar,
+  },
 }
 </script>
 
