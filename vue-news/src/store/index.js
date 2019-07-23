fb85b866be0251afa18178a4d0ae0f623aba96ea
ratseno <onestar1014@gmail.com>
2019-07-23 22:06:58 +0900

    [실습]mutation => ES6 Destructuring  적용(구조 분해 문법)

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -25,32 +25,35 @@ export const store = new Vuex.Store({
         FETCH_NEWS(context) {
             fetchNewsList()
                 .then(response => {
-                    console.log(response);
+
                     context.commit('SET_NEWS', response.data);
+
                 })
                 .catch(error => {
-                    console.log(error);
+
                 });
         },
         FETCH_ASK(context) {
             fetchAskList()
                 .then(response => {
-                    console.log(response);
+
                     context.commit('SET_ASK', response.data);
+
                 })
                 .catch(error => {
-                    console.log(error);
 
                 });
         },
-        FETCH_JOBS(context) {
+        //ES6 Destructuring
+        FETCH_JOBS({ commit }) {
             fetchJobsList()
-                .then(response => {
-                    console.log(response);
-                    context.commit('SET_JOBS', response.data);
+                .then(({ data }) => {
+
+                    commit('SET_JOBS', data);
+
                 })
                 .catch(error => {
-                    console.log(error);
+
                 });
         }
     }
